b405607789b090f7fb5c5a29d80eac95f42ed2b0
Brandon Chuck <blc42310@gmail.com>
2022-01-04 17:15:09 -0800

    Refactored all files;

--- calculator.js
+++ calculator.js
@@ -1,105 +1,72 @@
-// ---------- STATE VARIABLES ----------
 let display = document.querySelector("#display");
 let operand = "";
 let num1 = "";
 let num2 = "";
 let memory = "";
+let result;
 
-// ----- DOM ELEMENTS -----
-// Numbers
-document.querySelector("#zero").addEventListener("click", numberPressed);
-document.querySelector("#one").addEventListener("click", numberPressed);
-document.querySelector("#two").addEventListener("click", numberPressed);
-document.querySelector("#three").addEventListener("click", numberPressed);
-document.querySelector("#four").addEventListener("click", numberPressed);
-document.querySelector("#five").addEventListener("click", numberPressed);
-document.querySelector("#six").addEventListener("click", numberPressed);
-document.querySelector("#seven").addEventListener("click", numberPressed);
-document.querySelector("#eight").addEventListener("click", numberPressed);
-document.querySelector("#nine").addEventListener("click", numberPressed);
-document.querySelector("#decimal").addEventListener("click", numberPressed);
+const numberBtns = document.querySelectorAll(".number-btns");
+numberBtns.forEach((button) => button.addEventListener("click", numberPressed));
 
-// Operands
-document.querySelector("#add").addEventListener("click", operandPressed);
-document.querySelector("#subtract").addEventListener("click", operandPressed);
-document.querySelector("#divide").addEventListener("click", operandPressed);
-document.querySelector("#multiply").addEventListener("click", operandPressed);
-document.querySelector("#inverse").addEventListener("click", operandPressed);
-document
-  .querySelector("#square-root")
-  .addEventListener("click", operandPressed);
+const operatorBtns = document.querySelectorAll(".operator-btns");
+operatorBtns.forEach((button) =>
+  button.addEventListener("click", operatorPressed)
+);
 
-// Memory
-document.querySelector("#memory-clear").addEventListener("click", memoryClear);
-document
-  .querySelector("#memory-recall")
-  .addEventListener("click", memoryRecall);
-document
-  .querySelector("#memory-add")
-  .addEventListener("click", memoryAddSubtract);
-document
-  .querySelector("#memory-subtract")
-  .addEventListener("click", memoryAddSubtract);
-document.querySelector("#memory-store").addEventListener("click", memoryStore);
+const memoryBtns = document.querySelectorAll(".memory-btns");
+memoryBtns.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    switch (e.target.textContent) {
+      case "MC":
+        memory = "";
+        break;
+      case "MR":
+        memoryRecall();
+        break;
+      case "M+":
+      case "M-":
+        memoryAddSubtract(e);
+        break;
+      case "MS":
+        memoryStore();
+        break;
+    }
+  });
+});
 
-// Misc.
-document.querySelector("#sign").addEventListener("click", changeSign);
-document.querySelector("#equals").addEventListener("click", equalsPressed);
-document.querySelector("#clear").addEventListener("click", clear);
-
-// ---------- NUMBERS ----------
 function numberPressed(e) {
-  if (e.target.textContent === "." && display.textContent.includes(".")) {
-    return;
-  }
+  if (e.target.textContent === "." && display.textContent.includes(".")) return;
 
-  if (operand === "") {
+  if (!operand) {
     num1 += e.target.textContent;
     display.textContent = num1;
   } else {
     num2 += e.target.textContent;
     display.textContent = num2;
   }
-
-  console.log("num1 = " + num1);
-  console.log("num2 = " + num2);
 }
 
-// ---------- OPERANDS ----------
-function operandPressed(e) {
-  if (num1 === "") {
+function operatorPressed(e) {
+  if (!num1) {
     alert("Please enter a number.");
-  } else {
-    if (num2 !== "") {
-      // e.target.classList.toggle("active"); // testing showing active operand
-      let result = calculate(num1, num2, operand);
-      num1 = result;
-      operand = e.target.textContent;
-      display.textContent = result;
-      num2 = "";
-      console.log("num1 = " + num1);
-      console.log("num2 = " + num2);
-    } else {
-      // e.target.classList.toggle("active");
-      operand = e.target.textContent;
-    }
   }
 
-  console.log("Current operand: " + operand);
+  if (num2) {
+    num1 = calculate(num1, num2, operand);
+    operand = e.target.textContent;
+    display.textContent = num1;
+    num2 = "";
+  }
+
+  operand = e.target.textContent;
 }
 
-// ---------- CALCULATION ----------
+document.querySelector("#equals").addEventListener("click", equalsPressed);
 function equalsPressed() {
-  let result = calculate(num1, num2, operand);
-  num1 = "";
+  num1 = calculate(num1, num2, operand);
   num2 = "";
   operand = "";
-  display.textContent = result;
-
-  console.log("num1 = " + num1);
-  console.log("num2 = " + num2);
-  console.log("Current operand: " + operand);
-  console.log("Current memory: " + memory);
+  display.textContent = num1;
 }
 
 function calculate(num_1, num_2, operand) {
@@ -113,22 +80,23 @@ function calculate(num_1, num_2, operand) {
 
   switch (operand) {
     case "+":
-      return (num1 + num2).toString();
+      return num1 + num2;
     case "-":
-      return (num1 - num2).toString();
-    case "*":
-      return (num1 * num2).toString();
+      return num1 - num2;
+    case "x":
+      return num1 * num2;
     case "/":
-      return (num1 / num2).toString();
+      return num1 / num2;
     case "sqrt":
-      return Math.sqrt(num1).toFixed(4).toString();
+      return Math.sqrt(num1).toFixed(4);
     case "1/x":
-      return (1 / num1).toFixed(4).toString();
+      return (1 / num1).toFixed(4);
   }
 }
 
+document.querySelector("#sign").addEventListener("click", changeSign);
 function changeSign() {
-  if (num2 === "") {
+  if (!num2) {
     num1 = num1.includes("-") ? num1.replace("-", "") : "-" + num1;
     display.textContent = num1;
   } else {
@@ -137,45 +105,24 @@ function changeSign() {
   }
 }
 
-// ---------- MEMORY FUNCTIONS ----------
-function memoryClear() {
-  memory = "";
-  console.log("Current memory: " + memory);
-}
-
 function memoryRecall() {
-  if (memory === "") {
+  if (!memory) {
     display.textContent = 0;
     alert("Nothing stored in memory... enter a number then press MS to store");
     return;
   }
 
-  if (operand !== "") {
-    num2 = memory;
-  } else {
-    display.textContent = memory;
+  if (!num1) {
+    num1 = memory;
   }
-  console.log("num1 = " + num1);
-  console.log("num2 = " + num2);
-  console.log("Current memory: " + memory);
+  num2 = memory;
+  display.textContent = num2;
 }
 
 function memoryAddSubtract(e) {
-  if (memory !== "") {
+  if (memory) {
     let op = e.target.textContent === "M+" ? "+" : "-";
-    let result;
-
-    // cleaner way to write this logic?
-    if (num1 !== "" && num2 === "") {
-      result = calculate(memory, num1, op);
-    }
-
-    if (num2 !== "") {
-      result = calculate(memory, num2, op);
-    }
-
-    memory = result;
-    console.log("Current memory: " + memory);
+    memory = calculate(memory, num1, op);
   } else {
     alert("Nothing stored in memory... enter a number then press MS to store.");
     return;
@@ -183,37 +130,15 @@ function memoryAddSubtract(e) {
 }
 
 function memoryStore() {
-  if (num1 === "") {
-    alert("Please enter a number, then click MS to store.");
-  }
-
-  // cleaner way to write this logic?
-  // if num1 has been entered and user presses MS
-  if (num1 !== "" && num2 === "") {
-    memory = num1;
-    console.log("Current memory: " + memory);
-  }
-  // if user wants to store num2 in memory
-  else if (num1 !== "" && num2 !== "") {
-    memory = num2;
-    console.log("Current memory: " + memory);
-  }
-
-  // -- original logic --
-  // if (num1 !== "") {
-  //   memory = num1;
-  //   console.log("Current memory: " + memory);
-  // } else {
-  //   alert("Please enter a number, then click MS to store.");
-  // }
+  if (display.textContent === "0") return;
+  memory = display.textContent;
+  console.log({ memory });
 }
 
-// ---------- RESET ----------
+document.querySelector("#clear").addEventListener("click", clear);
 function clear() {
   display.textContent = "0";
   num1 = "";
   num2 = "";
   operand = "";
-  console.log("num1 = " + num1);
-  console.log("num2 = " + num2);
 }
